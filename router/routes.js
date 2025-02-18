0f7eb220039cd5cda3c9f8807a5a533e79f4f122
Gagandeep Singh <gagandeepsingh128548@gmail.com>
2025-02-18 15:17:06 +0530

    done subscription section

--- router/routes.js
+++ router/routes.js
@@ -3,8 +3,10 @@ const rootRouter = Router();
 
 import staffRouter from "./admin/staff/staffDetail.router.js";
 import superAdminRouter from "./superAdmin/superAdmin.router.js";
+import subscriptionRouter from "./superAdmin/subscription.router.js";
 
 rootRouter.use("/staff", staffRouter);
 rootRouter.use("/superAdmin", superAdminRouter);
+rootRouter.use("/subscription", subscriptionRouter);
 
 export default rootRouter;
