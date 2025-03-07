34715a72bd691261f0271b0770dd4bdffe616930
Shukal-01 <131428954+Shukal-01@users.noreply.github.com>
2025-03-07 17:53:29 +0530

    change in staff router

--- router/admin/staff/staffDetail.router.js
+++ router/admin/staff/staffDetail.router.js
@@ -1,11 +1,12 @@
 import express from "express";
 import { createStaff, getAllStaff, getStaffById, updateStaff, deleteStaff  } from "../../../controller/admin/staff/staffDetail.controller.js";
+import { authorizationMiddleware } from "../../../middleware/auth.js";
 const staffRouter = express.Router();
 
-staffRouter.post("/createStaff", createStaff);
-staffRouter.get("/", getAllStaff);
-staffRouter.get("/one", getStaffById);
-staffRouter.put("/:id", updateStaff);
-staffRouter.delete("/:id", deleteStaff);
+staffRouter.post("/createStaff", authorizationMiddleware, createStaff);
+staffRouter.get("/", authorizationMiddleware, getAllStaff);
+staffRouter.get("/one", authorizationMiddleware, getStaffById);
+staffRouter.put("/:id", authorizationMiddleware, updateStaff);
+staffRouter.delete("/:id", authorizationMiddleware, deleteStaff);
 
 export default staffRouter;
