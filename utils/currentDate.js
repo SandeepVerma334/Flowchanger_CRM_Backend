ae3e146d9c66ca746874d55bb59d58214c34d34b
SandeepVerma334 <87061839+SandeepVerma334@users.noreply.github.com>
2025-03-29 13:45:54 +0530

    Create currentDate.js

---
+++ utils/currentDate.js
@@ -0,0 +1,10 @@
+
+
+// Get current time in Indian Time (Asia/Kolkata)
+function getIndiaTime(date) {
+    const inputDate = date ? new Date(date) : new Date(); // Use provided date or current date
+    return new Date(inputDate.toLocaleString("en-US", { timeZone: "Asia/Kolkata" }));
+};
+
+
+export default getIndiaTime;
