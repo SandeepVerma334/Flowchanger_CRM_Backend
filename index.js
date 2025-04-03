ed6a468e26c12493994464c98daf04395594aedf
Gagandeep Singh <145465280+gagandeepsingh101@users.noreply.github.com>
2025-04-03 09:59:29 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,14 @@ app.use(express.static(buildPath));
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors({ origin: "*" }));
+app.use(
+  cors({
+    origin: "*", 
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
